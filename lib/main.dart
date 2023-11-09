5f014fb6b1fbd1402773f0e92774e9b93a768a2e
Matthew Pullen <matthew.pullen@gmail.com>
2023-11-09 16:21:09 -0600

    Created custom widgets and separatedinto files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import "package:flutter/material.dart";
+import "package:dice_app/gradient_container.dart";
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello World!!")));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
 }
